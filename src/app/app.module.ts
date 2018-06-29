4c556fdebba890998bd477cef00962f6656e023d
Hector Coello <hector_m3@live.com.mx>
2018-06-29 13:18:49 -0500

    Se agregó contenido y estructura al Body. Se agregaron ngModels a los inputs y se crearon variables.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,7 +13,8 @@ import { BodyComponent } from './components/body/body.component';
     BodyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
-export class BodyComponent implements OnInit {
+export class BodyComponent {
+
+  fruta_nueva = "";
+  frutas = ['Uva','Naranja'];
+  frutas_string = String(this.frutas);
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
